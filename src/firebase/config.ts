7ec3210a74a7664718f6f9b775e7006204c6aea4
Asiqur Rahman <ashiqurrahman248225@gmail.com>
2026-03-30 13:59:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7380996316-5f21a",
+  "appId": "1:1051226687044:web:44882f566708053f3b1e11",
+  "apiKey": "AIzaSyAFl0edVTRDVvTY5S69t_udBVcqDI0Uax4",
+  "authDomain": "studio-7380996316-5f21a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1051226687044"
+};
