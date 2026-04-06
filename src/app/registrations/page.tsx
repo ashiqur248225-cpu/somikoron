bbfba5aa4bade6102b995df974b3e397367a0e51
Asiqur Rahman <ashiqurrahman248225@gmail.com>
2026-04-06 05:31:12 +0000

    Try fixing this error: `Runtime ReferenceError: CheckCircle is not defin

--- src/app/registrations/page.tsx
+++ src/app/registrations/page.tsx
@@ -10,7 +10,7 @@ import { Badge } from "@/components/ui/badge"
 import { 
   UserCheck, XCircle, Loader2, Eye, Phone, Building2, 
   MapPin, GraduationCap, Calendar, Clock, Filter, Trash2, UserCircle, Briefcase,
-  AlertCircle, Calculator, Info, Utensils, Plus, Minus, History, Wallet
+  AlertCircle, Calculator, Info, Utensils, Plus, Minus, History, Wallet, CheckCircle
 } from "lucide-react"
 import { useToast } from "@/hooks/use-toast"
 import { useFirestore, useCollection, useMemoFirebase } from "@/firebase"
